4af6c41f641067013b0415772f1e1e92ef8ee3b3
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2019-08-26 17:52:03 +0100

    signin/signout mutation and resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -92,6 +92,36 @@ const Mutations = {
      // we return the user to the browser
      return user;
   },
+  async signin(parent, {email, password}, ctx, info) {
+    // check if there is a user with that email
+    const user = await ctx.db.query.user({
+      where: {email}
+    });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // check if there password is correct
+    const valid = bcrypt.compare(password, user.password);
+    if(!valid) {
+      throw new Error('Invalid password!');
+    }
+    // generate the JWT token
+    const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
+    // set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // return the user
+    return user;
+
+  },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return {
+      message: 'GoodBye!'
+    }
+  },
   async createList(parent, args, ctx, info) {
     const list = await ctx.db.mutation.createList(
       {
