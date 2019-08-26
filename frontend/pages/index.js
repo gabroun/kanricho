4e4826f8b8b038b33bfe2c2141801f97d2ba6e2e
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2019-08-26 23:28:17 +0100

    accountNav component, updating styles, and routing for signin and signout

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,16 @@
 import React from "react";
-import Boards from '../components/Dashboard/Boards'
+import Link from "next/link";
 const Home = props => (
   <div>
-    <Boards {...props}/>
+    <h2>A new way to manage your work</h2>
+    <p>By organising your projects into boards, lists and cards.</p>
+    <Link href="/signup">
+      <a>
+        <span className="header__title" style={{ color: "blue" }}>
+          Get Started
+        </span>
+      </a>
+    </Link>
   </div>
 );
 
